be8f901c256e704b2c87d18995131b28de90cca4
DannyNguyen <thaiduongnguyenngoc27@gmail.com>
2026-06-02 18:33:55 +0700

    Add files via upload

---
+++ xacdinhsonguyento-n.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+// câu 1
+using namespace std;
+
+int main() {
+    int n; // Khai báo biến n
+    cin >> n; // nhập giá trị cho n
+    bool nguyento;
+    if (n < 2) {
+        nguyento = false; // số nguyên tố phải lớn hơn 1
+    } else {
+        nguyento = true;
+        } int i = 2;
+        if (n > 2 && n % i == 0) {
+            nguyento = false; // số nguyên tố chỉ chia hết cho 1 và chính nó
+        }
+    if (nguyento == true) {
+        cout << "n là số nguyên tố";
+    } else {
+        cout << "n không phải là số nguyên tố";
+    }
+    return 0;
+}
